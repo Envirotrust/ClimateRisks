9c81f15fd384754962d026e8fd3167a8a9164ebb
sammygis <adedoyinsamuel25@gmail.com>
2025-06-21 22:58:21 +0100

    chore:set the local path config for fire_weather_risk

--- endpoints/core/config.py
+++ endpoints/core/config.py
@@ -1,2 +1,2 @@
 FLOOD_DATA = r'C:\Users\Admin\Desktop\ongoing_tasks\environtrust\ClimateRisks\data\flood.parquet'
-
+FIRE_WEATHER_RISK = r'C:\Users\Admin\Desktop\ongoing_tasks\environtrust\ClimateRisks\data\fire_weather_risk'
